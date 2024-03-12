47cf6c9558cc9db0b6abd3ad51a5ef155213c61f
MuhammadAfzal <muhammadafzaal@ezwage.com>
2024-03-12 10:59:46 +0500

    first commit

---
+++ lib/data/current_activity_mock.dart
@@ -0,0 +1,24 @@
+import '../models/current_activity.dart';
+
+List<Transaction> currentActivity = [
+  Transaction(
+      profileName: 'Ali',
+      amount: '\$105',
+      date: '09:40am 12/02-2023',
+      expenseType: 1),
+  Transaction(
+      profileName: 'Jack',
+      amount: '\$415',
+      date: '09:40am 12/02-2023',
+      expenseType: 2),
+  Transaction(
+      profileName: 'Jesse',
+      amount: '\$15',
+      date: '09:40am 12/02-2023',
+      expenseType: 1),
+  Transaction(
+      profileName: 'Pinkman',
+      amount: '\$25',
+      date: '09:40am 12/02-2023',
+      expenseType: 1)
+];
